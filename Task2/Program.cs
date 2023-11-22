d53a8c0a0a4f312b5d8560753174c6aa57ab199a
Emilishere <emilahu@code.edu.az>
2023-11-22 14:51:52 +0400

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Emil Huseynli --- Task 2 -- Completed: 21 November
+
+// There were easier methods to use, but this is the best practice for what we have covered so far
+
+#region first
+
+/*string name = "Emil";
+string surname = "Huseynli";
+Console.WriteLine("Candidate's Full Name: " + name + " " + surname);*/
+
+#endregion
+
+#region second
+
+/*int firstNumber = 3;
+int secondNumber = 5;
+int sum = firstNumber + secondNumber;
+Console.WriteLine("Sum of two numbers: " + sum);*/
+
+#endregion
+
+#region third
+/*int firstNumber = 3;
+int secondNumber = 5;
+int difference = firstNumber - secondNumber;
+Console.WriteLine("Difference of two numbers: " + difference);*/
+#endregion
+
+#region fourth
+/*int firstNumber = 2;
+int secondNumber = 3;
+int product = firstNumber * secondNumber;
+Console.WriteLine("Product of two numbers: " + product);
+Console.WriteLine("Square of the product: " + product * product);*/
+#endregion
+
+#region fifth
+/*float firstNumber = 18;
+float secondNumber = 5;
+float division = firstNumber/secondNumber;
+Console.WriteLine("The result is: " + division);
+Console.WriteLine("The square of the result is: " + division * division);*/
+#endregion
